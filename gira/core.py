4a69f2552fd98609eea7a528661998754c6f6083
Tomas Peterka <tomas.peterka@dronetag.cz>
2024-11-28 11:40:08 +0100

    refactor: use dataclasses

--- gira/core.py
+++ gira/core.py
@@ -1,51 +1,33 @@
+from dataclasses import dataclass, field
 from typing import Optional
 
 
+@dataclass(order=False, frozen=False)
 class Dependency:
     name: str
-    url: str
-    version: str
-
-    def __init__(self, name, url="", version=""):
-        self.name = name
-        self.url = url
-        self.version = version
+    url: str = ""
+    version: str = ""
 
     def __str__(self):
         return self.name
 
 
+@dataclass(order=False, frozen=True)
 class Change:
     name: str
     version: str
-    old: bool
-
-    def __init__(self, name, version, old):
-        self.name = name
-        self.version = version
-        self.old = old
+    old: bool = field(compare=False)
 
     def __str__(self):
         return f"{self.name}=={self.version}"
 
 
+@dataclass(order=False, frozen=False)
 class Upgrade:
     name: str
-    old_version: Optional[str]
-    new_version: Optional[str]
-    messages: Optional[list[str]]
-
-    def __init__(
-        self,
-        name: str,
-        old_version: Optional[str] = None,
-        new_version: Optional[str] = None,
-        messages=None,
-    ):
-        self.name = name
-        self.old_version = old_version
-        self.new_version = new_version
-        self.messages = messages
+    old_version: Optional[str] = None
+    new_version: Optional[str] = None
+    messages: list[str] = field(default_factory=list)
 
     def __str__(self):
         return f"{self.name} {self.old_version} => {self.new_version}:"
